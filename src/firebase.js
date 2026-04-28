ce24b780e3ccd9869af45817ecfede640198c34e
HelloSanjib <hellosanjib.369@gmail.com>
2026-04-28 23:18:22 +0530

    Add fallbacks for Firebase config to fix Vercel crash

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || "AIzaSyB81hrOj84I6eyWgc7RYiOU2Br8hvPyyck",
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || "nexus-rescue-5b27b.firebaseapp.com",
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || "nexus-rescue-5b27b",
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || "nexus-rescue-5b27b.firebasestorage.app",
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || "454012902294",
+  appId: process.env.REACT_APP_FIREBASE_APP_ID || "1:454012902294:web:28e630e72ef39b420a1bb7"
 };
 
 const app = initializeApp(firebaseConfig);
